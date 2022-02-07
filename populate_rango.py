1e989b7e6e959e57a37f6df482b060989abbb35c
Reetu-Varadhan <2492428v@student.gla.ac.uk>
2022-02-07 12:27:56 +0000

    chap6, two tests not working

--- populate_rango.py
+++ populate_rango.py
@@ -12,27 +12,35 @@ def populate():
 
     python_pages = [
         {'title': 'Official Python Tutorial',
-        'url': 'http://docs.python.org/3/tutorial/'},
+        'url': 'http://docs.python.org/3/tutorial/',
+        'views': 29},
         {'title': 'How to Think like a Computer Scientist',
-        'url': 'http://www.greenteapress.com/thinkpython/'},
+        'url': 'http://www.greenteapress.com/thinkpython/',
+        'views': 10},
         {'title': 'Learn Python in 10 Minutes',
-        'url': 'http://www.korokithakis.net/tutorials/python/'}
+        'url': 'http://www.korokithakis.net/tutorials/python/',
+        'views': 45}
     ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
-        'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01'},
+        'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01',
+        'views': 38},
         {'title': 'Django Rocks',
-        'url': 'http://www.djangorocks.com/'},
+        'url': 'http://www.djangorocks.com/',
+        'views': 42},
         {'title': 'How to Tango with Django',
-        'url': 'http://www.tangowithdjango.com/'}
+        'url': 'http://www.tangowithdjango.com/',
+        'views': 22}
     ]
 
     other_pages = [
         {'title': 'Bottle',
-        'url': 'http://bottlepy.org/docs/dev'},
+        'url': 'http://bottlepy.org/docs/dev',
+        'views': 8},
         {'title': 'Flask',
-        'url': 'http://flask.pocoo.org'}
+        'url': 'http://flask.pocoo.org',
+        'views': 80}
     ]
 
     cats = [{'name': 'Python', 'pages': python_pages, 'views': 128, 'likes': 64},
@@ -45,14 +53,14 @@ def populate():
     for cat_data in cats:
         c = add_cat(cat_data['name'], cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
     
     # print out the categories that have been added
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Page, Category
 
 def index(request):
+    # Query database for all categories currently stored, order them by # likes in descending order
+    # retrieve top 5 only
+    # place in context_dict
     # constructing a dictionary to pass to the template engine as its context
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
 
     # return a rendered httpresponse to the client
     return render(request, 'rango/index.html', context=context_dict)
@@ -11,4 +22,27 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Reetu V'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        #Tries to find a category name slug with the given name
+        # if DNE, the .get() method raises a DNE exception
+        category = Category.objects.get(slug=category_name_slug)
+
+        #Retrieve all associated pages
+        #Filater() returns a list of page objects, or an empty list
+        pages = Page.objects.filter(category=category)
+
+        #Adds results list to the template context
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
 
