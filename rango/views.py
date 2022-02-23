34ec65352a37503360b5d86f062fcd4d96e24d75
Reetu-Varadhan <2492428v@student.gla.ac.uk>
2022-02-23 10:47:31 +0000

    chap9 complete

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,8 @@ from rango.models import Page, Category
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # Query database for all categories currently stored, order them by # likes in descending order
@@ -50,6 +51,7 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -67,6 +69,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -156,6 +159,15 @@ def user_login(request):
 
     else:
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
 
 
 
